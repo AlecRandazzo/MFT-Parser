b87db22dfd0b7d997dbc751d98410361380fc9c1
AlecRandazzo <alec.randazzo@gmail.com>
2019-10-01 21:53:58 -0400

    more refactoring of my refactoring. ill be satisfied one day

--- header.go
+++ header.go
@@ -11,8 +11,11 @@ package GoFor_MFT_Parser
 
 import (
 	"errors"
+	"fmt"
 	bin "github.com/AlecRandazzo/BinaryTransforms"
 )
+
+type RawRecordHeader []byte
 
 type RecordHeader struct {
 	AttributesOffset uint16
@@ -20,50 +23,89 @@ type RecordHeader struct {
 	Flags            RecordHeaderFlags
 }
 
+type RawRecordHeaderFlag byte
+
 type RecordHeaderFlags struct {
 	FlagDeleted   bool
 	FlagDirectory bool
 }
 
-func (recordHeader *RecordHeader) Parse(mftRecord RawMasterFileTableRecord) (err error) {
-	if len(mftRecord) == 0 {
+func (rawRecordHeader RawRecordHeader) Parse() (recordHeader RecordHeader, err error) {
+	sizeOfRawRecordHeader := len(rawRecordHeader)
+
+	if sizeOfRawRecordHeader == 0 {
 		err = errors.New("RecordHeader.Parse() received nil bytes")
+		return
+	} else if sizeOfRawRecordHeader != 0x38 {
+		err = fmt.Errorf("RawRecordHeader.Parse() expected 38 bytes, instead it received %v", sizeOfRawRecordHeader)
 		return
 	}
 
 	const offsetRecordMagicNumber = 0x00
 	const lengthRecordMagicNumber = 0x05
-	magicNumber := string(mftRecord[offsetRecordMagicNumber : offsetRecordMagicNumber+lengthRecordMagicNumber])
+	magicNumber := string(rawRecordHeader[offsetRecordMagicNumber : offsetRecordMagicNumber+lengthRecordMagicNumber])
 	if magicNumber != "FILE0" {
 		err = errors.New("mftrecord missing magic number FILE0")
 		return
 	}
 
 	const offsetAttributesOffset = 0x14
-	const offsetRecordNumber = 0x2c
+	const offsetRecordNumber = 0x2C
 	const lengthRecordNumber = 0x04
-	const offsetRecordFlag = 0x16
 
-	recordHeader.AttributesOffset = uint16(mftRecord[offsetAttributesOffset])
-	recordHeader.Flags.Parse(mftRecord[offsetRecordFlag])
-	recordHeader.RecordNumber, _ = bin.LittleEndianBinaryToUInt32(mftRecord[offsetRecordNumber : offsetRecordNumber+lengthRecordNumber])
+	recordHeader.AttributesOffset = uint16(rawRecordHeader[offsetAttributesOffset])
+	rawRecordHeaderFlag, _ := rawRecordHeader.GetRawRecordHeaderFlags()
+
+	recordHeader.Flags = rawRecordHeaderFlag.Parse()
+	recordHeader.RecordNumber, _ = bin.LittleEndianBinaryToUInt32(rawRecordHeader[offsetRecordNumber : offsetRecordNumber+lengthRecordNumber])
 	return
 }
 
-func (recordHeaderFlags *RecordHeaderFlags) Parse(inByte byte) {
+func (rawRecordHeader RawRecordHeader) GetRawRecordHeaderFlags() (rawRecordHeaderFlag RawRecordHeaderFlag, err error) {
+	sizeOfRawRecordHeader := len(rawRecordHeader)
+
+	if sizeOfRawRecordHeader == 0 {
+		err = errors.New("received a nil bytes")
+		return
+	} else if sizeOfRawRecordHeader < 0x16 {
+		err = fmt.Errorf("expected at least 16 bytes, instead received %v", sizeOfRawRecordHeader)
+		return
+	}
+
+	const offsetRecordFlag = 0x16
+	rawRecordHeaderFlag = RawRecordHeaderFlag(rawRecordHeader[offsetRecordFlag])
+
+	return
+}
+
+func (rawRecordHeaderFlag RawRecordHeaderFlag) Parse() (recordHeaderFlags RecordHeaderFlags) {
 	const codeDeletedFile = 0x00
 	//const codeActiveFile = 0x01
 	//const codeDeletedDirectory = 0x02
 	const codeDirectory = 0x03
-	if inByte == codeDeletedFile {
+	if rawRecordHeaderFlag == codeDeletedFile {
 		recordHeaderFlags.FlagDeleted = true
 		recordHeaderFlags.FlagDirectory = false
-	} else if inByte == codeDirectory {
+	} else if rawRecordHeaderFlag == codeDirectory {
 		recordHeaderFlags.FlagDirectory = true
 		recordHeaderFlags.FlagDeleted = false
 	} else {
 		recordHeaderFlags.FlagDeleted = false
 		recordHeaderFlags.FlagDirectory = false
 	}
+	return
+}
+
+func (rawMftRecord RawMasterFileTableRecord) GetRawRecordHeader() (rawRecordHeader RawRecordHeader, err error) {
+	sizeOfRawMftRecord := len(rawMftRecord)
+	if sizeOfRawMftRecord == 0 {
+		err = errors.New("received nil bytes")
+		return
+	} else if sizeOfRawMftRecord < 0x38 {
+		err = fmt.Errorf("expected at least 38 bytes, instead received %v", sizeOfRawMftRecord)
+		return
+	}
+
+	rawRecordHeader = RawRecordHeader(rawMftRecord[0:0x38])
 	return
 }
--- header_test.go
+++ header_test.go
@@ -5,41 +5,32 @@ import (
 	"testing"
 )
 
-func TestRecordHeaderFlags_Parse(t *testing.T) {
-	type args struct {
-		inByte byte
-	}
+func TestRawRecordHeaderFlag_Parse(t *testing.T) {
 	tests := []struct {
-		name string
-		got  RecordHeaderFlags
-		args args
-		want RecordHeaderFlags
+		name                string
+		got                 RecordHeaderFlags
+		want                RecordHeaderFlags
+		rawRecordHeaderFlag RawRecordHeaderFlag
 	}{
 		{
-			name: "deleted file 0x00",
-			args: args{
-				inByte: 0x00,
-			},
+			name:                "deleted file 0x00",
+			rawRecordHeaderFlag: 0x00,
 			want: RecordHeaderFlags{
 				FlagDeleted:   true,
 				FlagDirectory: false,
 			},
 		},
 		{
-			name: "directory 0x03",
-			args: args{
-				inByte: 0x03,
-			},
+			name:                "directory 0x03",
+			rawRecordHeaderFlag: 0x03,
 			want: RecordHeaderFlags{
 				FlagDeleted:   false,
 				FlagDirectory: true,
 			},
 		},
 		{
-			name: "other 0x06",
-			args: args{
-				inByte: 0x06,
-			},
+			name:                "other 0x06",
+			rawRecordHeaderFlag: 0x06,
 			want: RecordHeaderFlags{
 				FlagDeleted:   false,
 				FlagDirectory: false,
@@ -48,7 +39,7 @@ func TestRecordHeaderFlags_Parse(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.got.Parse(tt.args.inByte)
+			tt.got = tt.rawRecordHeaderFlag.Parse()
 			if !reflect.DeepEqual(tt.got, tt.want) {
 				t.Errorf("Test %v failed \ngot = %v, \nwant = %v", tt.name, tt.got, tt.want)
 			}
@@ -56,24 +47,17 @@ func TestRecordHeaderFlags_Parse(t *testing.T) {
 	}
 }
 
-func TestRecordHeader_Parse(t *testing.T) {
-	//
-
-	type args struct {
-		mftRecord []byte
-	}
+func TestRawRecordHeader_Parse(t *testing.T) {
 	tests := []struct {
-		name    string
-		got     RecordHeader
-		args    args
-		wantErr bool
-		want    RecordHeader
+		name            string
+		rawRecordHeader RawRecordHeader
+		got             RecordHeader
+		wantErr         bool
+		want            RecordHeader
 	}{
 		{
-			name: "MFT record 0",
-			args: args{
-				mftRecord: RawMasterFileTableRecord([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 96, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 0, 0, 0, 104, 0, 0, 0, 0, 0, 24, 0, 0, 0, 3, 0, 74, 0, 0, 0, 24, 0, 1, 0, 5, 0, 0, 0, 0, 0, 5, 0, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 0, 64, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 4, 3, 36, 0, 77, 0, 70, 0, 84, 0, 0, 0, 0, 0, 0, 0, 128, 0, 0, 0, 120, 0, 0, 0, 1, 0, 64, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 63, 55, 5, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 51, 32, 200, 0, 0, 0, 12, 67, 109, 148, 1, 212, 133, 226, 1, 67, 54, 210, 0, 106, 250, 123, 9, 66, 253, 12, 241, 48, 8, 245, 66, 69, 99, 201, 78, 228, 8, 67, 97, 209, 0, 235, 81, 198, 1, 67, 218, 198, 0, 17, 228, 150, 1, 0, 0, 0, 176, 0, 0, 0, 72, 0, 0, 0, 1, 0, 64, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 176, 2, 0, 0, 0, 0, 0, 8, 160, 2, 0, 0, 0, 0, 0, 8, 160, 2, 0, 0, 0, 0, 0, 49, 43, 103, 244, 2, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 8, 16, 0, 0, 0, 0, 0, 0, 49, 1, 255, 255, 11, 17, 1, 255, 0, 0, 0, 0, 0, 0, 29, 7, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 29, 7}),
-			},
+			name:            "valid raw record header",
+			rawRecordHeader: RawRecordHeader([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0}),
 			want: RecordHeader{
 				AttributesOffset: 56,
 				RecordNumber:     0,
@@ -85,23 +69,97 @@ func TestRecordHeader_Parse(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "nil bytes",
-			args: args{
-				mftRecord: nil,
-			},
-			wantErr: true,
+			name:            "nil bytes",
+			rawRecordHeader: nil,
+			wantErr:         true,
 		},
 		{
-			name: "bytes does not start with FILE0",
-			args: args{
-				mftRecord: []byte{0, 73, 76, 69, 48},
-			},
-			wantErr: true,
+			name:            "bytes does not start with FILE0",
+			rawRecordHeader: RawRecordHeader([]byte{0, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0}),
+			wantErr:         true,
+		},
+		{
+			name:            "not 38 bytes",
+			rawRecordHeader: RawRecordHeader([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0}),
+			wantErr:         true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.got.Parse(tt.args.mftRecord)
+			var err error
+			tt.got, err = tt.rawRecordHeader.Parse()
+			if !reflect.DeepEqual(tt.got, tt.want) || (err != nil) != tt.wantErr {
+				t.Errorf("Test %v failed \ngot = %v, \nwant = %v", tt.name, tt.got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRawMasterFileTableRecord_GetRawRecordHeader(t *testing.T) {
+	tests := []struct {
+		name         string
+		rawMftRecord RawMasterFileTableRecord
+		want         RawRecordHeader
+		got          RawRecordHeader
+		wantErr      bool
+	}{
+		{
+			name:         "nil bytes",
+			rawMftRecord: nil,
+			wantErr:      true,
+		},
+		{
+			name:         "not enough bytes",
+			rawMftRecord: RawMasterFileTableRecord([]byte{0x01}),
+			wantErr:      true,
+		},
+		{
+			name:         "valid rawMftRecord",
+			rawMftRecord: RawMasterFileTableRecord([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 96, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 48, 0, 0, 0, 104, 0, 0, 0, 0, 0, 24, 0, 0, 0, 3, 0, 74, 0, 0, 0, 24, 0, 1, 0, 5, 0, 0, 0, 0, 0, 5, 0, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 234, 36, 205, 74, 116, 212, 209, 1, 0, 64, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 4, 3, 36, 0, 77, 0, 70, 0, 84, 0, 0, 0, 0, 0, 0, 0, 128, 0, 0, 0, 120, 0, 0, 0, 1, 0, 64, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 63, 55, 5, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 0, 0, 116, 83, 0, 0, 0, 0, 51, 32, 200, 0, 0, 0, 12, 67, 109, 148, 1, 212, 133, 226, 1, 67, 54, 210, 0, 106, 250, 123, 9, 66, 253, 12, 241, 48, 8, 245, 66, 69, 99, 201, 78, 228, 8, 67, 97, 209, 0, 235, 81, 198, 1, 67, 218, 198, 0, 17, 228, 150, 1, 0, 0, 0, 176, 0, 0, 0, 72, 0, 0, 0, 1, 0, 64, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 176, 2, 0, 0, 0, 0, 0, 8, 160, 2, 0, 0, 0, 0, 0, 8, 160, 2, 0, 0, 0, 0, 0, 49, 43, 103, 244, 2, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, 8, 16, 0, 0, 0, 0, 0, 0, 49, 1, 255, 255, 11, 17, 1, 255, 0, 0, 0, 0, 0, 0, 29, 7, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 29, 7}),
+			wantErr:      false,
+			want:         RawRecordHeader([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			tt.got, err = tt.rawMftRecord.GetRawRecordHeader()
+			if !reflect.DeepEqual(tt.got, tt.want) || (err != nil) != tt.wantErr {
+				t.Errorf("Test %v failed \ngot = %v, \nwant = %v", tt.name, tt.got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRawRecordHeader_GetRawRecordHeaderFlags(t *testing.T) {
+	tests := []struct {
+		name            string
+		rawRecordHeader RawRecordHeader
+		got             RawRecordHeaderFlag
+		want            RawRecordHeaderFlag
+		wantErr         bool
+	}{
+		{
+			name:            "valid raw record header",
+			rawRecordHeader: RawRecordHeader([]byte{70, 73, 76, 69, 48, 0, 3, 0, 155, 21, 101, 188, 33, 0, 0, 0, 1, 0, 1, 0, 56, 0, 1, 0, 200, 1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 29, 7, 0, 0, 0, 0, 0, 0}),
+			wantErr:         false,
+			want:            RawRecordHeaderFlag(0x01),
+		},
+		{
+			name:            "nil bytes",
+			rawRecordHeader: nil,
+			wantErr:         true,
+		},
+		{
+			name:            "not enough bytes",
+			rawRecordHeader: RawRecordHeader([]byte{0x00}),
+			wantErr:         true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var err error
+			tt.got, err = tt.rawRecordHeader.GetRawRecordHeaderFlags()
 			if !reflect.DeepEqual(tt.got, tt.want) || (err != nil) != tt.wantErr {
 				t.Errorf("Test %v failed \ngot = %v, \nwant = %v", tt.name, tt.got, tt.want)
 			}
