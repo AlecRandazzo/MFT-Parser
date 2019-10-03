4374683e6c005ca4340648a863c74c076a0f1ff1
AlecRandazzo <alec.randazzo@gmail.com>
2019-10-02 22:49:57 -0400

    more refactors for my refactors

--- standardinformation_attribute_test.go
+++ standardinformation_attribute_test.go
@@ -8,24 +8,18 @@ import (
 )
 
 func TestStandardInformationAttributes_Parse(t *testing.T) {
-	type args struct {
-		attribute Attribute
-	}
 	tests := []struct {
-		name    string
-		got     StandardInformationAttribute
-		args    args
-		want    StandardInformationAttribute
-		wantErr bool
+		name                            string
+		got                             StandardInformationAttribute
+		want                            StandardInformationAttribute
+		rawStandardInformationAttribute RawStandardInformationAttribute
+		wantErr                         bool
 	}{
 		{
-			name:    "test 1",
-			wantErr: false,
-			args: args{attribute: Attribute{
-				AttributeType:  0x10,
-				AttributeBytes: []byte{16, 0, 0, 0, 96, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 49, 147, 66, 169, 237, 209, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 44, 238, 221, 229, 226, 245, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 168, 220, 169, 88, 0, 0, 0, 0},
-				AttributeSize:  0,
-			}},
+			name:                            "test 1",
+			wantErr:                         false,
+			rawStandardInformationAttribute: []byte{16, 0, 0, 0, 96, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 49, 147, 66, 169, 237, 209, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 44, 238, 221, 229, 226, 245, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 168, 220, 169, 88, 0, 0, 0, 0},
+
 			want: StandardInformationAttribute{
 				SiCreated:    ts.TimeStamp(time.Date(2018, 4, 11, 23, 34, 40, 104324900, time.UTC)),
 				SiModified:   ts.TimeStamp(time.Date(2018, 4, 11, 23, 34, 40, 104324900, time.UTC)),
@@ -35,27 +29,20 @@ func TestStandardInformationAttributes_Parse(t *testing.T) {
 			},
 		},
 		{
-			name:    "nil bytes",
-			wantErr: true,
-			args: args{attribute: Attribute{
-				AttributeType:  0x10,
-				AttributeBytes: nil,
-				AttributeSize:  0,
-			}},
+			name:                            "nil bytes",
+			wantErr:                         true,
+			rawStandardInformationAttribute: nil,
 		},
 		{
-			name:    "non-resident",
-			wantErr: true,
-			args: args{attribute: Attribute{
-				AttributeType:  0x10,
-				AttributeBytes: []byte{16, 0, 0, 0, 96, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 49, 147, 66, 169, 237, 209, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 44, 238, 221, 229, 226, 245, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 168, 220, 169, 88, 0, 0, 0, 0},
-				AttributeSize:  0,
-			}},
+			name:                            "non-resident",
+			wantErr:                         true,
+			rawStandardInformationAttribute: []byte{16, 0, 0, 0, 96, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 72, 0, 0, 0, 24, 0, 0, 0, 49, 147, 66, 169, 237, 209, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 44, 238, 221, 229, 226, 245, 211, 1, 49, 147, 66, 169, 237, 209, 211, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 253, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 168, 220, 169, 88, 0, 0, 0, 0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.got.Parse(tt.args.attribute)
+			var err error
+			tt.got, err = tt.rawStandardInformationAttribute.Parse()
 			if !reflect.DeepEqual(tt.got, tt.want) || (err != nil) != tt.wantErr {
 				t.Errorf("Test %v failed \ngot = %v, \nwant = %v", tt.name, tt.got, tt.want)
 			}
